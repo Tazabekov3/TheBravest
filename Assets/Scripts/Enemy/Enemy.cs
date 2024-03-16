dc6bd70d84c3a3c4a84187443bc9ac2015d6fdbc
Tazabekov3 <dautaz@mail.ru>
2024-03-16 11:26:36 +0500

    second commit
    Добавлена возможность наносить урон, немного изменена иерархия скриптов

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) 
+        {
+            Debug.Log("I am dead");
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage) 
+    {
+        this.health -= damage;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public int damage = 2;
+    public float attackCooldown = 0.3f;
+    private float attackTimer;
+
+    public Transform attackPosition;
+    public float attackRange;
+    public LayerMask enemyLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackTimer <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                attackTimer = attackCooldown;
+                Collider2D[] enemiesHit = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, enemyLayer);
+                for (int i = 0; i < enemiesHit.Length; i++)
+                {
+                    enemiesHit[i].GetComponent<Enemy>().TakeDamage(damage);
+                    Debug.Log("Enemy Hit");
+                }
+            }
+        } else
+        {
+            attackTimer -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+    }
+}
